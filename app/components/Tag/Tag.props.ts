3fc82077589a6d74460853b721e7eeb55ff14e9c
zlatovvd <31865609+zlatovvd@users.noreply.github.com>
2024-04-18 23:56:05 +0300

    1804two

--- app/components/Tag/Tag.props.ts
+++ app/components/Tag/Tag.props.ts
@@ -1,8 +1,8 @@
 import { DetailedHTMLProps, HTMLAttributes, ReactNode } from "react";
 
 export interface TagProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
-    size?: 'm' | 'l';
+    size?: 'm' | 's';
     children: ReactNode;
-    color?: 'ghost' | 'red' | 'green' | 'grey' | 'primary';
+    color?: 'ghost' | 'red' | 'green' | 'gray' | 'primary';
     href?: string;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
 import styles from "./page.module.css";
-import { Button, Htag, P } from "./components";
+import { Button, Htag, P, Tag } from "./components";
 
 export default function Home(): JSX.Element {
   return (
-      <div>
-               <Htag tag={'h3'}>Text1</Htag> 
-               <Button appearance="primary" arrow="right">Кнопка</Button>
-               <Button appearance="ghost" arrow="down">Кнопка</Button>
-               <P size="l">Text p</P>
-      </div>
+    <div>
+      <Htag tag={"h3"}>Text1</Htag>
+      <Button appearance="primary" arrow="right">
+        Кнопка
+      </Button>
+      <Button appearance="ghost" arrow="down">
+        Кнопка
+      </Button>
+      <P size="l">Text p</P>
+      <Tag color="primary">primary</Tag>
+      <Tag color="green">green</Tag>
+      <Tag color="red">red</Tag>
+      <Tag color="gray">gray</Tag>
+      <Tag color="ghost">ghost</Tag>
+    </div>
   );
 }
