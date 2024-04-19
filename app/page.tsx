019142c158b9831df95bb0ca99629c09adb6861c
unknown <zlatovvd@ukr.net>
2024-04-19 15:46:53 +0300

    1904a

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
+"use client"
+
 import styles from "./page.module.css";
 import { Button, Htag, P } from "./components";
+import { useState } from "react";
 
 export default function Home(): JSX.Element {
+
+  const [counter, setCounter] = useState<number>(0)
+
   return (
       <div>
-               <Htag tag={'h3'}>Text1</Htag> 
-               <Button appearance="primary" arrow="right">Кнопка</Button>
+               <Htag tag={'h3'}>{counter}</Htag> 
+               <Button appearance="primary" arrow="right" onClick={()=>setCounter(x => x + 1)}>Кнопка</Button>
                <Button appearance="ghost" arrow="down">Кнопка</Button>
                <P size="l">Text p</P>
       </div>
